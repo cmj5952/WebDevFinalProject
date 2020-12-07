105e80c9fdd8dc514a9475707f43cc523cae7af3
cmj5952 <74368413+cmj5952@users.noreply.github.com>
2020-12-07 11:36:23 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,69 @@
- const redLight = document.getElementById('raceButton');
 
-
-redLight.addEventListener('click', race());
-
-function race (){
+const init = () => {
+    redRaceButton.addEventListener('click', start);
+    resetButton.addEventListener('click', reset);
+  }
+  
+  
+  const redRaceButton = document.getElementById('redRaceButton');
+  const greenRaceButton = document.getElementById('greenRaceButton');
+  const resetButton = document.getElementById('reset');
+  const cars = document.getElementsByClassName("cars");
+  const carBlue = document.getElementById('carBlue');
+  const carRed = document.getElementById('carRed');
+  const output = document.getElementById('output');
+  
+  
+  
+  const getRandomTime = () => {
+    return Math.floor((Math.random() * 3))* 150;
+  }
+  
+  
+  const calculateWinner = (carRedTime, carBlueTime) => {
+      if(carBlueTime < carRedTime){
+        console.log("Red won");
+            output.innerText = "Blue won!";
+      } else if(carBlueTime > carRedTime ){
+        output.innerText = "Red won!";
+      } else {
+        output.innerText = "Draw!"
+      }
+   
+    setTimeout( () => {
+        carBlue.classList.add('moveRight');
+  
+     }, carBlueTime,9000);
+  
+      setTimeout( () => {
+        carRed.classList.add('moveRight');
+  
+     }, carRedTime, 9000);
+  }
+  
+  const start = () => {
+      redRaceButton.style.display = "none";
+      greenRaceButton.style.display = "block";
+      resetButton.style.display = "block";
+      console.log("Reset button was clicked")
+  
+      
+      let carBlueTime = getRandomTime();
+      let carRedTime = getRandomTime();
     
-}
+      
+      calculateWinner(carRedTime, carBlueTime);
+  }
+  
+  const reset = () => {
+    carBlue.classList.remove('moveRight');
+    carRed.classList.remove('moveRight');
+  
+     redRaceButton.style.display = "block";
+     greenRaceButton.style.display = "none";
+     output.innerText = "";
+    resetButton.style.display = "";
+   
+  } 
+  
+  init();
